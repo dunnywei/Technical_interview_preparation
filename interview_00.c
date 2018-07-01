2149840a930cfdbf77c64e457db5d4c594b1d8ad
dunnywei <wei_don@yahoo.com>
2018-07-01 18:05:23 -0400

    interview_00 verifying done

--- interview_00.c
+++ interview_00.c
@@ -1,4 +1,30 @@
 /*
- *Verify a given string is valid IP address with range 0..255
+ *Q1:Verify a given string is valid IP address with range 0..255
  *Writie a uni test for this
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+
+
+int verify(char *input)
+{
+  int result=0;
+  int test=atoi(input);
+  if((test>=0)&&(test><=255)
+  {
+      return 1;
+  }
+   
+  return result;
+}
+
+
+int main ()
+{
+  char str[5]="132";
+  printf("The result is %d\n",verify(str));
+  
+}
