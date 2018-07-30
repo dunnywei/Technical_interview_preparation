e531cc8e777ef7bb99c17bc038ae13a7c47225a0
dunnywei <wei_don@yahoo.com>
2018-07-30 16:36:17 -0400

    update hacker_rank_C_q2.c

--- hacker_rank_C_q2.c
+++ hacker_rank_C_q2.c
@@ -6,15 +6,20 @@
 int main() 
 {
 
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+  /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
     char ch;
     char s[100];
+    char t[100];
+
     scanf("%c \n",&ch);
     printf("%c \n",ch);
     
-    scanf("%[^\n]%*c",s);
-    //scanf("%[^\n]%*c", s)
-    printf("%s",s);
+    scanf("%[^\n]%*c \n",s);
+    //scanf("%[^\n]%*c \n", s)
+    printf("%s \n",s);
+    
+    scanf("%[^\n]%*c \n",t);
+    printf("%s \n",t);
     return 0;
 }
 
